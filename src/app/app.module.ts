cb17a2e1898c08686355e54e5ebb7eccfa706316
yago batista da silva <yagobatistasilva@gmail.com>
2017-12-02 23:50:09 -0200

    remove componente nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import { NavComponent } from './nav/nav.component';
 import { HomeComponent } from './home/home.component';
 import { ProdutoComponent } from './produto/produto.component';
 import { CompraComponent } from './compra/compra.component';
@@ -12,7 +11,6 @@ import { CompraComponent } from './compra/compra.component';
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent,
     HomeComponent,
     ProdutoComponent,
     CompraComponent
