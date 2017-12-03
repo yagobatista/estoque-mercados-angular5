c1601af78255779ab6eaf3989fddee21e427ea15
yago batista da silva <yagobatistasilva@gmail.com>
2017-12-02 22:08:39 -0200

    criando component compra, produto e home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,18 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
+import { ProdutoComponent } from './produto/produto.component';
+import { CompraComponent } from './compra/compra.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent,
+    ProdutoComponent,
+    CompraComponent
   ],
   imports: [
     BrowserModule
