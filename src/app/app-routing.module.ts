e28b5ab232d7bb429a37a1d9c7665a4a47ebeef2
yago batista da silva <yagobatistasilva@gmail.com>
2017-12-03 00:29:19 -0200

    router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule }             from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+
+import { HomeComponent }      from './home/home.component';
+
+
+const routes: Routes = [
+  { path: '',component: HomeComponent }
+];
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
